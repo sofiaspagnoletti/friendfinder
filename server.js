5b2eb5911da5b46ca8b8e08e7252a666d9384ecf
sofi.spag <sofi.spag@gmail.com>
2019-07-08 15:48:54 -0700

    adding html structure

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ var express = require ('express');
 var app = express();
 var path = require("path");
 
-//CHANGE THE PORT AND GO TO PACKAGE JASON AND 'START'
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.send('hello world')
-})
+// app.get('/', function(req, res){
+//     res.send('hello world')
+// })
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 
-// NEXT STEPS: 
-// then type in terminal heroku login
-// then git remote -y
-// heroku create and the name we want
-// git remote -v
-// (git add, commit) + git push heroku master
-// heroku open
