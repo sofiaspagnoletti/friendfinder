4174fe7b344f512a5d556eea9077506aaf7b19d2
sofi.spag <sofi.spag@gmail.com>
2019-07-08 16:49:22 -0700

    adding survey questions

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-var express = require ('express');
+var express = require("express");
 var app = express();
-var path = require("path");
 
 var PORT = process.env.PORT || 3000;
 
-// app.get('/', function(req, res){
-//     res.send('hello world')
-// })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routing/apiRoutes")(app);
+// require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
