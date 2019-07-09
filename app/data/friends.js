af94d3eddea3f4d42e7459627c6970b58e7c16a0
sofi.spag <sofi.spag@gmail.com>
2019-07-08 22:03:52 -0700

    app.post working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,71 @@
 //add array of objects 
-// json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
+
+var friendArray = [
+    {
+        name: "Saima",
+        photo: "https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            3,
+            4,
+            5,
+            4
+        ]
+    },
+    {
+        name: "Ahmed",
+        photo: "https://s-s.huffpost.com/contributors/cameron-keady/headshot.jpg",
+        scores: [
+            4,
+            3,
+            4,
+            5,
+            5,
+            5,
+            3,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Patricia",
+        photo: "https://sovereignsilver.com/wp-content/uploads/2018/10/happy-life-blog-e1540999564867.jpg",
+        scores: [
+            4,
+            5,
+            5,
+            5,
+            5,
+            4,
+            4,
+            4,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Linda",
+        photo: "https://media.istockphoto.com/photos/be-yourself-and-your-confidence-will-shine-through-picture-id610678308?k=6&m=610678308&s=612x612&w=0&h=TG-Xp_d-xj_zUR5cQ3T2s5pDMY4245cty6SQEJi8PjE=",
+        scores: [
+            2,
+            5,
+            5,
+            1,
+            5,
+            4,
+            1,
+            4,
+            1,
+            3
+        ]
+    }
+];
+
+module.exports = friendArray;
+
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,2 +1,29 @@
-// A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// A POST routes `/api/friends`. This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility logic.
+
+var friendsData = require("../app/data/friends");
+
+module.exports = function(app) {
+   
+    app.get("/api/friends", function(req, res) {
+      res.json(friendsData);
+    });
+  
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        console.log(newFriend);
+        friendsData.push(newFriend);
+        res.json(newFriend);
+      });
+    
+  
+  };
+
+// function getSum(total, scores) {
+//     const scores = parseInt(friendArray.scores);
+//     return total + Math.round(scores);
+// }
+
+// getSum();
+
